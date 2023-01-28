19088a88f391a9fcf57ffc180d31ade25e651d3a
Michael-Burton1 <76928439+Michael-Burton1@users.noreply.github.com>
2023-01-28 13:29:37 -0800

    first commit with api call

---
+++ Api_call/API.js
@@ -0,0 +1,14 @@
+
+// function getData(
+//   console.log(fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population')
+//   .then((response) => response.json())
+//   .then((data) => console.log(data)))
+
+// );
+
+// console.log(getData())
+
+
+console.log(fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population')
+.then((response) => response.json())
+.then((data) => console.log(data)))
