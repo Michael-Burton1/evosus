0e09b4f5115bfd43fe94a8f2de56cb11fe902798
Michael-Burton1 <76928439+Michael-Burton1@users.noreply.github.com>
2023-01-28 22:19:56 -0800

    add second state to state pop chart

--- frontend/index.js
+++ frontend/index.js
@@ -56,10 +56,10 @@ fetchStateData();
     let res2 = await fetchStateData();
     let populationData2 = res2.data;
     let oregon = populationData2.filter((item => item.State === 'Oregon'));
-    let newYork = populationData2.filter((item => item.State === 'New York'));
+    let newYork = populationData2.filter((item => item.State === 'Maryland'));
     let orYearsNeeded2 = oregon.filter((item => item.Year < 2020 && item.Year > 2012))
     console.log('OR', orYearsNeeded2);
-    let nyYearsNeeded2 = oregon.filter((item => item.Year < 2020 && item.Year > 2012))
+    let nyYearsNeeded2 = newYork.filter((item => item.Year < 2020 && item.Year > 2012))
     console.log('OR', nyYearsNeeded2);
 
       var data = new google.visualization.DataTable();
@@ -86,7 +86,9 @@ fetchStateData();
             title: 'Population'
           },
           colors: ['#a52714'],
-          width:700
+          width:700,
+          height:400
+          
         };
         
         var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
@@ -95,35 +97,37 @@ fetchStateData();
         
         var data2 = new google.visualization.DataTable();
         data2.addColumn('string', 'year');
-        data2.addColumn('number', 'OR pop ','NY pop');
-        // data2.addColumn('number', 'NY Pop');
+        data2.addColumn('number', 'OR pop');
+        data2.addColumn('number', 'MD Pop');
         
         let populationArray2 = [];
         console.log('before', populationArray2);
         
         for(i=orYearsNeeded2.length-1; i>=0 ; i--){
-        populationArray2.push([orYearsNeeded2[i].Year, orYearsNeeded2[i].Population, nyYearsNeeded2[i].Population]);
-        console.log('inside step'+i, populationArray2);
-      }
-      console.log('after', populationArray2);
+          populationArray2.push([orYearsNeeded2[i].Year, orYearsNeeded2[i].Population, nyYearsNeeded2[i].Population]);
+          console.log('inside step'+i, populationArray2);
+        }
+        console.log('after', populationArray2);
+        
+        data2.addRows(
+          populationArray2
+          );
 
-      data2.addRows(
-        populationArray2
-      );
-
-      var options = {
-        hAxis: {
-          title: 'Year'
-        },
-        vAxis: {
-          title: 'Population'
-        },
+          var options = {
+            hAxis: {
+              title: 'Year'
+            },
+            vAxis: {
+              title: 'Population'
+            },
         colors: ['#a52714', '#000077'],
-        width:700
+        width:700,
+        height:400
       };
-
+      
       var chart2 = new google.visualization.LineChart(document.getElementById('chart_div2'));
       console.log(chart2)
       chart2.draw(data2, options);
     }
-
+    
+    
